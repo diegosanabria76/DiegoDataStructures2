c54fae35372652fcc020e08ae1c410b6871ac68e
diego sanabria <diegosanabria76@gmail.com>
2021-10-15 16:09:04 -0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,17 +1,69 @@
-def calcsquare(num):
-    print(num**2)
+class DiegoProgram:
+    num1 = 0
+    num2 = 0
+    def __init__(self):
 
+        self.tricks = []
 
-print("More calculations")
-num1= int(input("please enter number"))
-num2= int(input("please second number"))
+    def calcsquare(self, num1):
+        square = int(num1**2)
+        print(square)
 
-def add(num1,num2):
+    def add(self):
+        print("More calculations")
+        num1 = int(input("please enter number"))
+        num2 = int(input("please second number"))
 
-    return  num1 + num2
+        mesj = "la suma de ", num1, " mas ", num2, " es : ", num1 + num2
+        return mesj
 
+    def usernames(self):
+        usernames = ["joey tribanni", "monica gallaguer", "Chandler bing", "Phoebe Buffay", "novita ", "Luou louo",
+                     "nana",
+                     "gata", " diego", "stephi joncas", "denis republica dom", "ferdinando", "sin covid "]
 
-mesj = "la suma de ", num1, " mas ",num2," es : "
+        for i in range(len(usernames)):
+            usernames[i] = usernames[i].upper().replace("I", "&")
 
-print(mesj,  add(num1,num2))
+        print(usernames)
+
+    def enter_usernames(self):
+        size = int(input("entra numero de usuario que quieres agregar"))
+        usernames = ["diego", "nana"]
+
+        for i in range(0, size):
+            name = input("entra un nombre")
+            usernames.append(name)
+
+        print(usernames)
+        option = int(input("entra cero para salir y uno para continuar"))
+        while option != 0:
+            print(usernames)
+            name = input("entra un nombre a borrar")
+            usernames.remove(name)
+            print(usernames)
+            option = int(input("entra cero para salir y uno para continuar"))
+
+        print("has salido, gracias por usar el programa")
+
+if __name__ == '__main__':
+
+    test = DiegoProgram()
+    option = int(input("entra cero para salir y uno para continuar"))
+
+    while option != 0:
+        menu = int(input("1-Suma, 2-square"))
+        if menu == 1:
+
+            suma = test.add()
+            print(suma)
+
+        elif menu ==2:
+            numero = int(input("enter a number"))
+            test.calcsquare(numero)
+
+        option = int(input("entra cero para salir y uno para continuar"))
+
+    print("Thanks for using this piece of software")
+
 
