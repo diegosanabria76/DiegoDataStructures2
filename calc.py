f46d8e5b8d6af796bf726c3ef8b52094808b0f92
Diego Sanabria <SANABRID@csspo.gouv.qc.ca>
2021-10-15 08:56:34 -0400

    commint from Intellij new instalation

---
+++ calc.py
@@ -0,0 +1,17 @@
+def calcsquare(num):
+    print(num**2)
+
+
+print("More calculations")
+num1= int(input("please enter number"))
+num2= int(input("please second number"))
+
+def add(num1,num2):
+
+    return  num1 + num2
+
+
+mesj = "la suma de ", num1, " mas ",num2," es : "
+
+print(mesj,  add(num1,num2))
+
